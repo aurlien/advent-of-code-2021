1a7924ac24523abba02bb479d61dbee1ed91825f
Kristian Aurlien <kristian.aurlien@cognite.com>
2021-12-05 12:55:13 +0100

    Utility functions

--- src/utils.kt
+++ src/utils.kt
@@ -3,3 +3,7 @@ import java.io.File
 fun readInput(day: Int) =  File("src/day$day/input.txt").readLines()
 
 fun readSample(day: Int) =  File("src/day$day/sample.txt").readLines()
+
+fun String.bitsToInt() = Integer.parseInt(this, 2)
+
+fun List<Char>.bitsToInt() = joinToString("").bitsToInt()
